9af06cfb36af02d97e468fb12eff209c57b74e7d
cosgab <cossu.gabriele@gmail.com>
2018-06-30 21:59:27 +0200

    prima della chiusura di Pebbel

---
+++ src/pkjs/config.js
@@ -0,0 +1,78 @@
+module.exports = [
+  {
+    "type": "heading",
+    "defaultValue": "GC Watch Configuration"
+  },
+  {
+    "type": "text",
+    "defaultValue": "Scegli i colori."
+  },
+  {
+    "type": "section",
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "Colori"
+      },
+      {
+        "type": "color",
+        "messageKey": "backgroundColor",
+        "defaultValue": "0x000000",
+        "label": "Sfondo esterno BT ok"
+      },
+      {
+        "type": "color",
+        "messageKey": "backgroundBTColor",
+        "defaultValue": "0xFFFFFF",
+        "label": "Sfondo esterno con errore BT"
+      },
+      {
+        "type": "color",
+        "messageKey": "textColor",
+        "defaultValue": "0xFFFFFF",
+        "label": "Colore testo passi e data"
+      },
+      {
+        "type": "color",
+        "messageKey": "batteryColor",
+        "defaultValue": "0xFFFFFF",
+        "label": "Colore batteria"
+      },
+      {
+        "type": "color",
+        "messageKey": "hourColor",
+        "defaultValue": "0xFFFFFF",
+        "label": "Colore testo ore"
+      },
+      {
+        "type": "color",
+        "messageKey": "quadrantColor",
+        "defaultValue": "0xFFFFFF",
+        "label": "Colore quadrante"
+      },
+      {
+        "type": "color",
+        "messageKey": "innerFillColor",
+        "defaultValue": "0xFFFFFF",
+        "label": "Colore sfondo interno"
+      },
+      {
+        "type": "color",
+        "messageKey": "hourhandColor",
+        "defaultValue": "0xFFFFFF",
+        "label": "Colore lancetta ore"
+      },
+      {
+        "type": "color",
+        "messageKey": "minhandColor",
+        "defaultValue": "0xFFFFFF",
+        "label": "Colore lancetta minuti"
+      }
+
+    ]
+  },
+  {
+    "type": "submit",
+    "defaultValue": "Salva"
+  }
+];
